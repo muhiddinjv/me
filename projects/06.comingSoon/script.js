98ae2a9efd5075b451d55a513df9e583e71ca648
iMbassador <50528874+iMbassador@users.noreply.github.com>
2019-09-22 16:01:46 +0500

    landing page to 09.landingpage

--- projects/06.comingSoon/script.js
+++ projects/06.comingSoon/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 30, 2019 23:59:00").getTime();
+var countDownDate = new Date("Dec 31, 2019 23:59:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
