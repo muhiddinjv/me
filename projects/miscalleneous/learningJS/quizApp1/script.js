215f601ce653ec4516e3fe8af8ef0e44d7380dc0
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-04-10 11:19:53 +0500

    this is not easy
    I'm not sure what's going on

--- projects/miscalleneous/learningJS/quizApp1/script.js
+++ projects/miscalleneous/learningJS/quizApp1/script.js
@@ -1,29 +1,79 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-const shuffledQuestions, currentQuestionIndex;
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion();
+});
 
 function startGame() {
-    alert('started');
+    // alert('started');
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5)
         // Math.random = random number between 1 and 0
-        // -.5 gives a number above or less than 0 50% of the time which is a random array
+        // -.5 gives a number above or less than 0 50% of the time which is a random array. It simply shuffles the questions.
+    currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
-    setNextQuestion();
+};
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        };
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    })
+};
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+
+
+function resetState() {
+    nextButton.classList.add('hidden');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+};
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+    nextButton.classList.remove('hide');
+};
+
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+};
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
-
-
-
-function setNextQuestion() {}
-
-
-
-
-function selectAnswer() {}
-
 
 const questions = [{
     question: 'What is 2 + 2 ?',
