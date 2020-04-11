3c949fc5d1502a261406c248450da92cfa1e239b
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-04-11 11:00:47 +0500

    quiz-level-1
    wrote the lines //-11 to //-23 in javascript

--- projects/023.quizApps/quiz-level-1/script.js
+++ projects/023.quizApps/quiz-level-1/script.js
@@ -1,11 +1,12 @@
 const questionNumberSpan = document.querySelector(".question-num-value"); //-7
-const totalQuestionSpan = document.querySelector(".total-quesiton"); //-8
+const totalQuestionSpan = document.querySelector(".total-question"); //-8
 const question = document.querySelector(".question"); //-6
 const op1 = document.querySelector(".option1"); //-1
 const op2 = document.querySelector(".option2"); //-2
 const op3 = document.querySelector(".option3"); //-3
 const op4 = document.querySelector(".option4"); //-4
-let questionIndex = 0;
+let questionIndex = 0; //-10
+let index = 0; //-15
 
 // questions and options and answers
 
@@ -32,10 +33,25 @@ const questions = [{ //-5
 }]
 
 // set questions and options and question number
+totalQuestionSpan.innerHTML = questions.length; //-18
 function load() {
-    question.innerHTML = questions[questionIndex].q;
+    questionNumberSpan.innerHTML = index + 1; //-16
+    question.innerHTML = questions[questionIndex].q; //-10
+    op1.innerHTML = questions[questionIndex].options[0]; //-11
+    op2.innerHTML = questions[questionIndex].options[1]; //-12
+    op3.innerHTML = questions[questionIndex].options[2]; //-13
+    op4.innerHTML = questions[questionIndex].options[3]; //-14
+    index++; //-17
+}
+
+function randomQuestion() { //-19
+    let randomNumber = Math.floor(Math.random() * questions.length); //-22
+    questionIndex = randomNumber; //-23
+    // console.log(questionIndex);
+    load(); //-20
 }
 
 window.onload = function() {
-    load(); //-9
+    // load(); //-9
+    randomQuestion() //-21
 }
