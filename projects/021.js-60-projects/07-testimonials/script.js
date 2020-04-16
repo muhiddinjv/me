a33954f87920eeab94796292743f36827b3edf34
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-04-16 12:58:07 +0500

    project 7 - testimonials: changed the quotes

--- projects/021.js-60-projects/07-testimonials/script.js
+++ projects/021.js-60-projects/07-testimonials/script.js
@@ -1,18 +1,15 @@
 const quotes = [{
-    name: 'Stephen King',
-    quote: 'Get busy living or get busy dying'
+    name: 'Thomas Edison',
+    quote: 'Opportunity is missed by most people, because it is dressed in overalls and looks like work'
 }, {
-    name: 'John F. Kennedy',
-    quote: 'Those who dare to fail miserabley can achieve greatly'
+    name: 'George Bernard Shaw',
+    quote: 'Progress is impossible without change, and those who cannot change their minds cannot change anything'
 }, {
     name: 'Abraham Lincoln',
     quote: 'I\'m a success today because I had a friend who believed in me and I didnt have the heart to let him down'
 }, {
     name: 'Leonardo Da Vinci',
     quote: 'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happend to things.'
-}, {
-    name: 'Leo Tolstoy',
-    quote: 'If you want to be happy, be.'
 }, {
     name: 'Muhiddin',
     quote: 'For some people, life is a battle. For others, its a test. But to me life is a miracle. If one is curious enough, s/he can literally work wonders.'
@@ -22,18 +19,18 @@ const images = document.querySelector('.images')
 const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn')
 const customer = document.querySelector('.customer')
-const quote = document.querySelector('#quote')
+const text = document.querySelector('#text')
 
 let counter = 0;
-prevBtn.addEventListener('click', changeQuote)
+// prevBtn.addEventListener('click', changeQuote)
 prevBtn.addEventListener('click', prevImg)
 nextBtn.addEventListener('click', nextImg)
 
-function changeQuote() {
-    let random = Math.floor(Math.random() * quotes.length)
-    customer.innerHTML = quotes[random].name;
-    text.innerHTML = quotes[random].quote;
-}
+// function changeQuote() {
+//     let random = Math.floor(Math.random() * quotes.length)
+//     customer.innerHTML = quotes[random].name;
+//     text.innerHTML = quotes[random].quote;
+// }
 
 function nextImg() {
     images.animate([{
@@ -49,6 +46,7 @@ function nextImg() {
     }
     counter++;
     images.style.backgroundImage = `url(img/bg-${counter}.jpg)`
+        // text.innerHTML = quotes[1, 2];
 }
 
 function prevImg() {
