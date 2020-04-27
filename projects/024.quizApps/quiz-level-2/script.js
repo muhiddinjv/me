daa615b61df8e4b464d44c92ceb8371362eaa2f7
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-04-27 20:12:06 +0500

    create carousel for vocabulary

--- projects/024.quizApps/quiz-level-2/script.js
+++ projects/024.quizApps/quiz-level-2/script.js
@@ -1,18 +1,27 @@
-const quotes = [{
-    vocab: 'Thomas Edison',
+const newWords = [{
+    vocab: '(1) <b>Es</b> - <i>is</i>',
     video: "vid/voc-1.mp4"
 }, {
-    vocab: 'George Bernard Shaw',
+    vocab: '(2) <b>Va</b> - <i>goes</i>',
     video: "vid/voc-2.mp4"
 }, {
-    vocab: 'Abraham Lincoln',
+    vocab: '(3) <b>Quiere</b> - <i>wants</i>',
     video: "vid/voc-3.mp4"
 }, {
-    vocab: 'Leonardo Da Vinci',
+    vocab: '(4) <b>Compra</b> - <i>buys</i>',
     video: "vid/voc-4.mp4"
 }, {
-    vocab: 'Muhiddin',
+    vocab: '(5) <b>Jugador</b> - <i>player</i>',
     video: "vid/voc-5.mp4"
+}, {
+    vocab: '(6) <b>Zapatos</b> - <i>shoes</i>',
+    video: "vid/voc-6.mp4"
+}, {
+    vocab: '(7) <b>La tienda</b> - <i>the store</i>',
+    video: "vid/voc-7.mp4"
+}, {
+    vocab: '(8) <b>Un dia</b> - <i>1 day</i>',
+    video: "vid/voc-8.mp4"
 }]
 
 const prevBtn = document.querySelector('.prevBtn')
@@ -25,19 +34,19 @@ prevBtn.addEventListener('click', prevVid)
 nextBtn.addEventListener('click', nextVid)
 
 function nextVid() {
-    if (counter === 4) {
+    if (counter === 8) {
         counter = -1;
     }
     counter++;
-    customer.innerHTML = quotes[counter].vocab;
-    vidDiv.src = quotes[counter].video;
+    words.innerHTML = newWords[counter].vocab;
+    vidDiv.src = newWords[counter].video;
 }
 
 function prevVid() {
     if (counter === 0) {
-        counter = 5;
+        counter = 9;
     }
     counter--;
-    customer.innerHTML = quotes[counter].vocab;
-    vidDiv.src = quotes[counter].video;
+    words.innerHTML = newWords[counter].vocab;
+    vidDiv.src = newWords[counter].video;
 }
