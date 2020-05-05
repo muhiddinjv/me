ce215e343a93613a6842cf41e16c0078cccb9d75
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-05-05 18:01:02 +0500

    add a new project (quiz level 3) to my js projects

--- projects/024.quizApps/quiz-level-3/end.js
+++ projects/024.quizApps/quiz-level-3/end.js
@@ -1,19 +1,29 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-console.log(highScores);
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    console.log(username.value);
     saveScoreBtn.disabled = !username.value;
 
 })
 saveHighScore = e => {
-    console.log('clicked save');
     e.preventDefault();
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/')
+
+};
