961bcf6e68d385ea5826958d223e74a1dfa6587f
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-07-07 17:36:13 +0500

    add columns and rows to the table (done)

--- projects/miscalleneous/millionCoders/Milestone-2/pixelArt/designs.js
+++ projects/miscalleneous/millionCoders/Milestone-2/pixelArt/designs.js
@@ -1,8 +1,30 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const pixelCanvas = document.getElementById('pixelCanvas');
+const sizePicker = document.getElementById('sizePicker');
+const submit = document.getElementById('submit');
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+submit.addEventListener('click', (e) => {
+        e.preventDefault(); // dont refresh after submitting
+        // console.log(inputHeight.value, inputWidth.value);
+        // console.log(colorPicker.value);
+        pixelCanvas.firstChild.remove();
+        makeGrid(inputHeight, inputWidth);
+    })
+    // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height, width) {
     // Your code goes here!
+    console.log(height.value, width.value);
+    for (let i = 0; i < height.value; i++) {
+        let row = document.createElement('tr');
+        for (let x = 0; x < width.value; x++) {
+            let tbody = document.createElement('td');
+            row.appendChild(tbody);
+        }
+        pixelCanvas.appendChild(row);
+    }
 }
