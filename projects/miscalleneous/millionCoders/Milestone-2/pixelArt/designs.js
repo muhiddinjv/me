5afff6869b49407919d8b5b3fa654b1901f6e06f
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-07-08 18:30:12 +0500

    complete pixel art maker project

--- projects/miscalleneous/millionCoders/Milestone-2/pixelArt/designs.js
+++ projects/miscalleneous/millionCoders/Milestone-2/pixelArt/designs.js
@@ -1,24 +1,24 @@
 // Select color input
-const colorPicker = document.getElementById('colorPicker');
-const inputHeight = document.getElementById('inputHeight');
-const inputWidth = document.getElementById('inputWidth');
-const pixelCanvas = document.getElementById('pixelCanvas');
-const sizePicker = document.getElementById('sizePicker');
-const submit = document.getElementById('submit');
+const colorPicker = document.querySelector('#colorPicker');
+const inputHeight = document.querySelector('#inputHeight');
+const inputWidth = document.querySelector('#inputWidth');
+const pixelCanvas = document.querySelector('#pixelCanvas');
+const sizePicker = document.querySelector('#sizePicker');
+const submit = document.querySelector('#submit');
+const row = document.getElementsByTagName('td');
 // Select size input
 
 submit.addEventListener('click', (e) => {
-        e.preventDefault(); // dont refresh after submitting
+        e.preventDefault(); // stops refresh after submitting
         // console.log(inputHeight.value, inputWidth.value);
         // console.log(colorPicker.value);
-        pixelCanvas.firstChild.remove();
+        // pixelCanvas.removeChild();
         makeGrid(inputHeight, inputWidth);
     })
     // When size is submitted by the user, call makeGrid()
 
 function makeGrid(height, width) {
     // Your code goes here!
-    console.log(height.value, width.value);
     for (let i = 0; i < height.value; i++) {
         let row = document.createElement('tr');
         for (let x = 0; x < width.value; x++) {
@@ -26,5 +26,10 @@ function makeGrid(height, width) {
             row.appendChild(tbody);
         }
         pixelCanvas.appendChild(row);
+    }
+    for (let i = 0; i < row.length; i++) {
+        row[i].addEventListener('click', (e) => {
+            row[i].style.background = colorPicker.value;
+        })
     }
 }
