ab9c94fd1272b68d5a518360b2d81f74aec3132e
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-07-22 18:54:50 +0500

    update 15.wordScramble project

--- projects/028.jsProjects/000.fiftyProjects/15.wordScramble/script.js
+++ projects/028.jsProjects/000.fiftyProjects/15.wordScramble/script.js
@@ -9,7 +9,25 @@ btn.addEventListener('click', function() {
         inplay = true;
         btn.innerHTML = "Guess";
         inp.classList.toggle("hidden");
+        createWord();
     }
-    let random = Math.floor(Math.random() * myArr.length);
-    msg.innerHTML = myArr[random].split("");
 })
+
+function createWord() {
+    let random = Math.floor(Math.random() * myArr.length);
+    let letters = myArr[random].split("");
+    let rand = randomArray(letters)
+    console.log(rand);
+    // randomArray(letters);
+    return letters;
+}
+
+function randomArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const el = arr[i];
+        let r = Math.floor(Math.random() * (i + 1));
+        console.log(el);
+        console.log(r);
+        console.log(i);
+    }
+}
