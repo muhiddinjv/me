1f2c1c9d1275926a49dae660511a34ecba6cf9e0
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-07-31 12:18:53 +0500

    update portfolio

--- 29-twitter/backend/index.js
+++ 29-twitter/backend/index.js
@@ -1,14 +1,14 @@
 let express = require('express');
-let app = express(); 
+let app = express();
 let cors = require('cors');
-let bodyParser = require('body-parser')
+// let bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
   res.json({
