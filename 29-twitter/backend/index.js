1529b8b76b09918598bb3d6b1ecbea4a760283dc
iMbassador <50528874+iMbassador@users.noreply.github.com>
2020-08-05 14:30:20 +0500

    almost complete twitter project

--- 29-twitter/backend/index.js
+++ 29-twitter/backend/index.js
@@ -1,21 +1,26 @@
-// let bodyParser = require('body-parser');
 const express = require('express');
+// turn input into string
 const cors = require('cors');
+// automatically refresh db
 const monk = require('monk');
+const Filter = require('bad-words');
+// cannot use profanity
+const rateLimit = require('express-rate-limit');
+//limit to 1 request ever 15 seconds
 
 const app = express();
-const db = monk('localhost/twitter');
+const db = monk(process.env.MONGO_URI || 'localhost/twitter');
 const twits = db.get('twits');
+const filter = new Filter();
 
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+
 app.listen(5000, ()=> console.log('listening on http://localhost:5000'))
 
 app.get('/', (req,res)=>{
   res.json({
-    message: 'tweeetoooo'
+    message: 'tweeeteee'
   });
 });
 
@@ -32,18 +37,25 @@ function isValidTwit(twit){
   twit.content && twit.content.toString().trim() !== '';
 }
 
+app.use(rateLimit({
+  windowMs: 10 * 1000, // 10 seconds
+  max: 1 // Limit each IP to 1 request per windowMs
+}));
+
 app.post('/twits', (req,res)=>{
+  console.log(req.body);
   // Validation will keep your database(db) safe from bad stuff such as injections from hackers. So ALWAYS validate info!
   if (isValidTwit(req.body)){
     // insert into database(db)
     const twit = {
-      name: req.body.name.toString(),
-      content: req.body.content.toString(),
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
       created: new Date()
     };
     twits.insert(twit).then(createdTwit =>{
       res.json(createdTwit);
     })
+    // console.log(twit);
   } else {
     // respond with an error
     res.status(422);
